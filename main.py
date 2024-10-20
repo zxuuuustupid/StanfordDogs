5c38b9c440bbc168df37bb7a1c6da54ab3b2a318
Zxuuuu <1521786784@qq.com>
2024-10-20 18:41:56 +0800

    a stupid mistake

--- main.py
+++ main.py
@@ -14,7 +14,6 @@ transform = transforms.Compose([
             transforms.ToTensor(),
             transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
 ])
-
 if __name__ == '__main__':
     # 创建Dataset实例
     dataset = SimpleImageFolderDataset(root_dir, transform=transform)

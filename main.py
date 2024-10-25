5feea9eae056b65ff96f53ef7c677b3990bde19b
Zxuuuu <1521786784@qq.com>
2024-10-25 23:27:04 +0800

    add plt

--- main.py
+++ main.py
@@ -5,18 +5,28 @@ from triplet_loss import TripletLoss
 from torch.utils.data import DataLoader
 from torchvision.transforms import transforms
 
-from stanford.CNN import CNN
-from stanford.VGG import VGG
+from stanford.models.CNN import CNN
+from stanford.models.VGG import VGG
+from stanford.models.ResNet import ResNet18
 from stanford.dataset import SimpleImageFolderDataset
 from draw import process_show
 
 root_dir = './Images/train'
 root_dir2 = './Images/test'
+model_name = 'VGG'
+epoch_num = 50
 # 定义变换操作
 transform = transforms.Compose([
     transforms.Resize((256, 256)),
     transforms.ToTensor(),
-    transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
+    transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
+    transforms.RandomHorizontalFlip(),  # 随机水平镜像
+    transforms.RandomErasing(scale=(0.04, 0.2), ratio=(0.5, 2)),
+    transforms.RandomHorizontalFlip(),
+    transforms.RandomVerticalFlip(),  # 随机垂直翻转
+    transforms.RandomRotation(10),
+    # 随机遮挡
+    # transforms.RandomCrop(32, padding=4)
 ])
 if __name__ == '__main__':
     # 创建Dataset实例
@@ -30,25 +40,30 @@ if __name__ == '__main__':
     device = torch.device("cuda:0")
     print("load the model...")
     # model = CNN().to(device)
-    model = VGG().to(device)
+    # model = ResNet18().to(device)
+    model=VGG().to(device)
     # criterion = TripletLoss()
-    criterion = nn.CrossEntropyLoss()
-    optimizer = optim.SGD(model.parameters(), lr=0.001, momentum=0.9, nesterov=True, weight_decay=5e-4)
 
-    loss_list=[]
-    train_acclist=[]
-    test_acclist=[]
+    criterion = nn.CrossEntropyLoss()
+    optimizer = optim.Adam(model.parameters(), lr=0.001, betas=(0.9,0.999), weight_decay=5e-4)
+    # optimizer = optim.SGD(model.parameters(), lr=0.001, momentum=0.9, nesterov=True, weight_decay=5e-4)
+    loss_list = []
+    train_acclist = []
+    test_acclist = []
     # 现在你可以在训练循环中使用data_loader了
-    for epoch in range(1,21):
+    for epoch in range(1, epoch_num + 1):
         train_loss = 0
         train_acc = 0
         model.train()
+        if epoch%10 == 0:
+            optimizer.param_groups[0]['lr'] *= 0.1
 
         for images, label in data_loader1:
             img = images.to(device)
             label = label.to(device)
             out = model(img)
             loss = criterion(out, label)
+            
 
             optimizer.zero_grad()
             loss.backward()
@@ -79,6 +94,7 @@ if __name__ == '__main__':
         loss_list.append(train_loss)
 
         print("Epoch:", epoch, "loss:{:.4f}  train acc:{:.4f}  test acc:{:.4f}".format(train_loss
-              , train_acc / len(data_loader1), test_acc / len(data_loader2)))
+                                                                                       , train_acc / len(data_loader1),
+                                                                                       test_acc / len(data_loader2)))
 
-    process_show("VGG", list(range(1, 21)),loss_list,train_acclist, test_acclist)
+    process_show(model_name, list(range(1, epoch_num + 1)), loss_list, train_acclist, test_acclist)

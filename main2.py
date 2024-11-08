ebaacf2d419c7212fe338412dc52d6db06d062b4
Zxuuuu <1521786784@qq.com>
2024-11-08 13:07:00 +0800

    add transfer learning

--- main2.py
+++ main2.py
@@ -2,8 +2,6 @@ import torch
 import matplotlib.pyplot as plt
 from torch import nn, optim
 from torchvision.models import ResNet18_Weights
-
-from triplet_loss import TripletLoss
 from torch.utils.data import DataLoader
 from torchvision.transforms import transforms
 from torchvision import models
@@ -17,20 +15,17 @@ root_dir = './Images/train'
 root_dir2 = './Images/test'
 model_name = 'Pretrained resnet'
 epoch_num = 20
+
 # 定义变换操作
 transform = transforms.Compose([
     transforms.Resize((256, 256)),
     transforms.ToTensor(),
     transforms.Normalize(mean=[0.485, 0.456, 0.406],
-                         std=[0.229, 0.224, 0.225]
-                         ),
+                         std=[0.229, 0.224, 0.225]),
     transforms.RandomHorizontalFlip(),  # 随机水平镜像
-    # transforms.RandomErasing(scale=(0.04, 0.2), ratio=(0.5, 2)),
-    # transforms.RandomVerticalFlip(),  # 随机垂直翻转
     transforms.RandomRotation(10),
-    # 随机遮挡
-    # transforms.RandomCrop(32, padding=4)
 ])
+
 if __name__ == '__main__':
     # 创建Dataset实例
     dataset = SimpleImageFolderDataset(root_dir, transform=transform)
@@ -42,39 +37,37 @@ if __name__ == '__main__':
 
     device = torch.device("cuda:0")
     print("load the model...")
-    # model = CNN().to(device)
-    # model = ResNet18().to(device)
-    # model = VGG().to(device)
-    weights = ResNet18_Weights.DEFAULT  # 或者选择具体的版本，如 IMAGENET1K_V1
+
+    weights = ResNet18_Weights.DEFAULT  # 使用默认的预训练权重
     model = models.resnet18(weights=weights).to(device)
 
-    # criterion = TripletLoss()
-
+    # 定义损失函数和优化器
     criterion = nn.CrossEntropyLoss()
-    # optimizer = optim.Adam(model.parameters(), lr=0.001, betas=(0.9, 0.999), weight_decay=5e-4)
 
+    # 先冻结卷积层，只训练全连接层
     for param in model.parameters():
-        param.requires_grad_=False
+        param.requires_grad = False
 
+    # 替换全连接层
     fc_in_features = model.fc.in_features
-    model.fc = nn.Linear(fc_in_features, 10, bias=True).to(device)
+    model.fc = nn.Linear(fc_in_features, 10, bias=True).to(device)  # 10类（斯坦福狗数据集）
 
     optimizer = optim.SGD(model.fc.parameters(), lr=0.001, momentum=0.9, nesterov=True, weight_decay=5e-4)
+
     loss_list = []
     train_acclist = []
     test_acclist = []
 
-    # 现在你可以在训练循环中使用data_loader了
+    # 第一阶段：冻结卷积层，训练全连接层
     for epoch in range(1, epoch_num + 1):
         train_loss = 0
         train_acc = 0
         model.train()
-        if epoch == 10:
-            optimizer.param_groups[0]['lr'] *= 0.1
 
         for images, label in data_loader1:
             img = images.to(device)
             label = label.to(device)
+
             out = model(img)
             loss = criterion(out, label)
 
@@ -94,6 +87,7 @@ if __name__ == '__main__':
         for images2, label2 in data_loader2:
             img = images2.to(device)
             label = label2.to(device)
+
             out = model(img)
             loss = criterion(out, label)
 
@@ -106,8 +100,60 @@ if __name__ == '__main__':
         test_acclist.append(test_acc / len(data_loader2))
         loss_list.append(train_loss)
 
-        print("Epoch:", epoch, "loss:{:.4f}  train acc:{:.4f}  test acc:{:.4f}".format(train_loss
-                                                                                       , train_acc / len(data_loader1),
-                                                                                       test_acc / len(data_loader2)))
+        print("Epoch:", epoch, "loss:{:.4f}  train acc:{:.4f}  test acc:{:.4f}".format(
+            train_loss, train_acc / len(data_loader1), test_acc / len(data_loader2)))
 
+    # 第二阶段：解冻深层卷积层（layer4），继续训练
+    for param in model.layer4.parameters():
+        param.requires_grad = True
+
+    # 重新设置优化器，包含解冻的层
+    optimizer = optim.SGD(filter(lambda p: p.requires_grad, model.parameters()), lr=0.0001, momentum=0.9, nesterov=True, weight_decay=5e-4)
+
+    # 继续训练
+    for epoch in range(epoch_num + 1, epoch_num + 21):  # 继续训练20个epoch
+        train_loss = 0
+        train_acc = 0
+        model.train()
+
+        for images, label in data_loader1:
+            img = images.to(device)
+            label = label.to(device)
+
+            out = model(img)
+            loss = criterion(out, label)
+
+            optimizer.zero_grad()
+            loss.backward()
+            optimizer.step()
+
+            train_loss += loss.item()
+
+            _, pred = out.max(1)
+            num_correct = (pred == label).sum().item()
+            acc = num_correct / img.shape[0]
+            train_acc += acc
+
+        test_acc = 0
+        model.eval()
+        for images2, label2 in data_loader2:
+            img = images2.to(device)
+            label = label2.to(device)
+
+            out = model(img)
+            loss = criterion(out, label)
+
+            _, pred = out.max(1)
+            num_correct = (pred == label).sum().item()
+            acc = num_correct / img.shape[0]
+            test_acc += acc
+
+        train_acclist.append(train_acc / len(data_loader1))
+        test_acclist.append(test_acc / len(data_loader2))
+        loss_list.append(train_loss)
+
+        print("Epoch:", epoch, "loss:{:.4f}  train acc:{:.4f}  test acc:{:.4f}".format(
+            train_loss, train_acc / len(data_loader1), test_acc / len(data_loader2)))
+
+    # 绘制训练过程中的损失和准确率曲线
     process_show(model_name, list(range(1, epoch_num + 1)), loss_list, train_acclist, test_acclist)
